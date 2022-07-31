4f3e1b6c704657eb0cf4a61b0bc714c205d725fc
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-30 20:11:20 -0500

    first commit

---
+++ Notes Imitation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes Imitation
+//
+//  Created by Camilo Hernández Guerrero on 30/07/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
